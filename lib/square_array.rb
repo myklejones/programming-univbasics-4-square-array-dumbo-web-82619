6d7664f5acbc61038b78dc8080b755e3bbadc8cb
Mykle jones <mykle.j91@gmail.com>
2019-08-12 04:31:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,17 @@
+#def square_array(array)
+ # count=0
+ #while array do
+#Math.exp(2)
+#count+=1
+# end 
+#end
+
 def square_array(array)
   count=0
-  while array do
-Math.exp(2)
-count+=1
- end 
+  squared=[]
+while count < array.length do
+squared<<(array[count]**2)
+ count+=1
+ end
+ squared
 end
